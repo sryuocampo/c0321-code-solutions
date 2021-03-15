260a696e43c05d8bed48fe8b510a7c0dcb968c5d
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-03-15 12:04:10 -0700

    Completed JS-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+
+var fullName = 'Sarah Ryu Ocampo';
+var isCool = true;
+var totalPets = 1;
+var unknown;
+var never =
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof 'Sarah Ryu Ocampo');
+console.log('typeof is cool:', typeof true);
+console.log('typeof totalPets:', typeof 1);
+console.log('typeof unknown:', typeof unknown);
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
