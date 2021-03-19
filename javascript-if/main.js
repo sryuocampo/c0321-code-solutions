32cfe476d8f01ebebcff48f0117025961f846a71
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-03-18 17:10:26 -0700

    Completed javascript-if.

---
+++ javascript-if/main.js
@@ -0,0 +1,74 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number) {
+  if (number % 2) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function startsWithJ(string) {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrink(person) {
+  if (person.age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrive(person) {
+  if (person.age >= 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  if (person) {
+    return false;
+  }
+}
+
+function categorizeAcidity(pH) {
+  if (pH < 0 || pH > 14) {
+    return 'invalid pH level';
+  }
+  if (pH < 7) return 'acid';
+  if (pH > 7) return 'base';
+  return 'neutral';
+}
+
+function introduceWarnerBro(name) {
+  if (name === 'yakko' || name === 'wakko') {
+    return 'We are the Warner brothers!';
+  } else if (name === 'dot') {
+    return "I'm cute~";
+  } else {
+    return 'Goodnight everybody!';
+  }
+}
