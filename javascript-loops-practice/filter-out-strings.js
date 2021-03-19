adc9508a5a5119724e15e34cec3aadd446f34c14
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-03-19 10:57:11 -0700

    Updated loops-practice.

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -3,7 +3,7 @@
 function filterOutStrings(values) {
   var filter = [];
   for (var i = 0; i < values.length; i++) {
-    if (typeof values[i] === 'number') {
+    if (typeof values[i] !== 'string') {
       filter.push(values[i]);
     }
   }
