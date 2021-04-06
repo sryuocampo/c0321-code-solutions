380ade6c25c94bad708a63b22557155a0b15ff2c
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-04-05 21:29:58 -0700

    Completed javascript-custom-methods.

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,34 @@
+/* exported calculator */
+
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  square: function (x) {
+    return x * x;
+  },
+  sumAll: function (numbers) {
+    var total = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      total = total + numbers[i];
+    }
+    return total;
+  },
+  getAverage: function (numbers) {
+    var total = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+    total = total / numbers.length;
+    return total;
+  }
+};
