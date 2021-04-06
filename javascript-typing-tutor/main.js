db63ee2c76a0fd57ee17a30f6c6d59a1b92e083b
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-04-05 19:51:34 -0700

    still working.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,1 +1,10 @@
+var $span = document.querySelectorAll('span');
 
+for (var i = 0; i < $span.length; i++) {}
+var currentPosition = 0;
+
+window.addEventListener('keydown', function (event) {
+  if (event.key === $span[currentPosition].textContent) {
+    currentPosition++;
+  }
+});
