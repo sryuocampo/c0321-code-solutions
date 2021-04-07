14798be67ed739e0261a1f21f861242a87b23445
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-04-07 14:35:27 -0700

    still working.

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,17 @@
 /* exported Bank */
+
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (Number.isInteger(balance) && balance > 0) {
+    var newAccount = new Account(holder, this.nextAccountNumber);
+    this.accounts.push(newAccount);
+    // this.accounts.push(accountObj);
+    // this.nextAccountNumber++;
+    // return Account.number;
+  }
+  return null;
+};
