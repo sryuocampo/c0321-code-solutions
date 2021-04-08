5aec90acfc872c3b2a1600548999e0d5f1d0c6dd
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-04-08 00:05:27 -0700

    Completed javascript-bank.

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -7,11 +7,30 @@ function Bank() {
 
 Bank.prototype.openAccount = function (holder, balance) {
   if (Number.isInteger(balance) && balance > 0) {
-    var newAccount = new Account(holder, this.nextAccountNumber);
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(balance);
     this.accounts.push(newAccount);
-    // this.accounts.push(accountObj);
-    // this.nextAccountNumber++;
-    // return Account.number;
+    this.nextAccountNumber++;
+    return newAccount.number;
   }
   return null;
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (number === this.accounts[i].number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var totalSum = 0;
+
+  for (var i = 0; i < this.accounts.length; i++) {
+    var account = this.accounts[i];
+    totalSum += account.getBalance();
+  }
+  return totalSum;
 };
