32c89bde63d075d4f707cfbc4ab59a4689cc80ee
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-04-08 00:59:16 -0700

    Completed javascript-typing-tutor.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,10 +1,18 @@
 var $span = document.querySelectorAll('span');
 
-for (var i = 0; i < $span.length; i++) {}
 var currentPosition = 0;
 
 window.addEventListener('keydown', function (event) {
-  if (event.key === $span[currentPosition].textContent) {
+  if (
+    $span[currentPosition] !== undefined &&
+    event.key === $span[currentPosition].textContent
+  ) {
+    $span[currentPosition].className = 'correct';
     currentPosition++;
+    if ($span[currentPosition] !== undefined) {
+      $span[currentPosition].className = 'next';
+    }
+  } else if ($span[currentPosition] !== undefined) {
+    $span[currentPosition].className = 'error';
   }
 });
