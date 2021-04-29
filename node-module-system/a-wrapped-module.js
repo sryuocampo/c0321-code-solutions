32ccfbb01c12ce2bf2e9434214a5b890e6582ee1
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-04-29 12:03:42 -0700

    Completed, after way to long, node module system.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+const typeExports = typeof exports;
+console.log('typeof Exports', typeExports);
+
+const typeRequire = typeof require;
+console.log('typeof require', typeRequire);
+
+const typeModule = typeof module;
+console.log('typeof module', typeModule);
+
+const typeFileName = typeof __filename;
+console.log('typeof filename', typeFileName);
+
+const typeDirName = typeof __dirname;
+console.log('typeof dirname', typeDirName);
