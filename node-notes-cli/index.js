cb44adefed3091380fb5144cd1f2368ad9dceade
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-05-06 10:36:14 -0700

    Working on delete.

--- node-notes-cli/index.js
+++ node-notes-cli/index.js
@@ -1,25 +1,35 @@
 const fs = require('fs');
 
-const done = setTimeout(() => console.log('Program took too long to complete.'), 30000);
+const done = setTimeout(
+  () => console.log('Program took too long to complete.'),
+  30000
+);
 
 fs.readFile('data.json', 'utf8', (err, result) => {
   if (err) throw err;
   const data = JSON.parse(result);
 
-  for (const id in data.notes) {
-    console.log(`${id}: ${data.notes[id]}`);
+  if (process.argv[2] === 'read') {
+    for (const id in data.notes) {
+      console.log(`${id}: ${data.notes[id]}`);
+    }
   }
 
-  // add notes if process.argv[]
-
-  const note = process.argv[3]; // my note text
-  const id = data.nextId++; // 5
-
-  data.notes[id] = note;
-
-  console.log(data);
-
   fs.writeFile('data.json', JSON.stringify(data, null, 4), 'utf8', () => {
+    if (process.argv[2] === 'create') {
+      const note = process.argv[3];
+      const id = data.nextId++;
+
+      data.notes[id] = note;
+
+      console.log(`${id}: ${data.notes[id]}`);
+    } else if (process.argv[2] === 'delete') {
+      const deleteId = process.argv[3];
+
+      if (deleteId === data.notes.id) {
+        delete data.notes;
+      }
+    }
     clearTimeout(done);
   });
 });
