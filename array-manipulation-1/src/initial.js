1bb3c00af1b16d3736565bd0facf5b1155d5799a
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-05-10 10:12:00 -0700

    Still working array-manipulations-1.

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,1 +1,8 @@
 /* exported initial */
+function initial(input) {
+  var array = [];
+  for (var i = 0; i < input.length; i++) {
+    array.push(input[i]);
+  }
+  return input;
+}
--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -2,12 +2,8 @@
 
 function tail(input) {
   var array = [];
-  for (var i = 0; i < input.length; i++) {
-    if (i === input[0]) {
-      return;
-    } else {
-      array.push(input[i]);
-    }
+  for (var i = 1; i < input.length; i++) {
+    array.push(input[i]);
   }
   return array;
 }
