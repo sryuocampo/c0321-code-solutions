1a639732004daaf6c75d2f9d7cc174a77726bc97
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-05-11 12:59:52 -0700

    Completed array map.

---
+++ array-map/main.js
@@ -0,0 +1,32 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const languages = [
+  'Hypertext Markup Language',
+  'Cascading Style Sheets',
+  'ECMAScript',
+  'PHP Hypertext Preprocessor',
+  'Structured Query Language'
+];
+
+const doubled = numbers.map(nums => {
+  return nums * 2;
+});
+
+console.log(doubled);
+
+const prices = numbers.map(nums => {
+  return '$' + nums.toFixed(2);
+});
+
+console.log(prices);
+
+const upperCased = languages.map(letters => {
+  return letters.toUpperCase(letters[0]);
+});
+
+console.log(upperCased);
+
+const firstLetters = languages.map(letter => {
+  return letter[0];
+});
+console.log(firstLetters);
