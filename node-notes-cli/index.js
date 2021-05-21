cd8f8fbcbcdf94c98c652dcecb7e39edf4333118
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-05-21 00:15:02 -0700

    Completed node notes cli.

--- node-notes-cli/index.js
+++ node-notes-cli/index.js
@@ -13,25 +13,27 @@ fs.readFile('data.json', 'utf8', (err, result) => {
     for (const id in data.notes) {
       console.log(`${id}: ${data.notes[id]}`);
     }
+    clearTimeout(done);
+    return;
+  }
+
+  if (process.argv[2] === 'create') {
+    const note = process.argv[3];
+    const id = data.nextId++;
+
+    data.notes[id] = note;
+
+    console.log(`${id}: ${data.notes[id]}`);
+  } else if (process.argv[2] === 'delete') {
+    const deleteId = process.argv[3];
+    delete data.notes[deleteId];
+  } else if (process.argv[2] === 'update') {
+    const updateId = process.argv[3];
+    const note = process.argv[4];
+    data.notes[updateId] = note;
   }
 
   fs.writeFile('data.json', JSON.stringify(data, null, 4), 'utf8', () => {
-    if (process.argv[2] === 'create') {
-      const note = process.argv[3];
-      const id = data.nextId++;
-
-      data.notes[id] = note;
-
-      console.log(`${id}: ${data.notes[id]}`);
-    } else if (process.argv[2] === 'delete') {
-      const deleteId = process.argv[3];
-
-      if (deleteId === data.notes.id) {
-        delete data.notes;
-      }
-    }
     clearTimeout(done);
   });
 });
-
-// delete
