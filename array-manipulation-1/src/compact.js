a9eff6367466e6fb30bcbb1de788e9fb300fff66
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-05-21 01:20:48 -0700

    Completed array manipulation 1.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,4 +1,10 @@
 /* exported compact */
-function compact(array) {
+function compact(input) {
+  var array = [];
+  for (var i = 0; i < input.length; i++) {
+    if (input[i]) {
+      array.push(input[i]);
+    }
+  }
   return array;
 }
