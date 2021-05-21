19769b1aaff4b04e7a62a186c1286ed39420480e
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-05-20 23:40:55 -0700

    Completed javascript carousel.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -3,7 +3,7 @@ var images = [
   'images/004.png',
   'images/007.png',
   'images/025.png',
-  'images/039.png',
+  'images/039.png'
 ];
 
 var numImages = images.length;
@@ -24,33 +24,75 @@ function previousImage() {
   }
 }
 
-function bubbles(index) {
-  currentPosition = index;
-}
-
 function getCurrentImage() {
   return images[currentPosition];
 }
 
 function drawCircles() {
-  var divCircles = document.getElementsByClassName('circles');
+  var divCircles = document.getElementById('circles');
 
-  var notSelected = document.createElement('i');
-  notSelected.setAttribute('class', 'fas fa-circle');
+  while (divCircles.children[0]) {
+    divCircles.removeChild(divCircles.children[0]);
+  }
 
-  var spanNotSelected = document.createElement('span');
-  spanNotSelected.setAttribute('class', 'not-selected');
-
-  spanNotSelected.appendChild(notSelected);
-
-  var selected = document.createElement('i');
-  selected.setAttribute('class', 'far fa-circle');
-
-  var spanSelected = document.createElement('span');
-  spanSelected.setAttribute('class', 'selected');
-
-  spanSelected.appendChild(selected);
-
-  divCircles.append(spanNotSelected, spanSelected);
+  for (let i = 0; i < numImages; i++) {
+    if (i === currentPosition) {
+      var selected = document.createElement('i');
+      selected.setAttribute('class', 'fas fa-circle');
+      divCircles.appendChild(selected);
+    } else {
+      var notSelected = document.createElement('i');
+      notSelected.setAttribute('class', 'far fa-circle');
+      divCircles.appendChild(notSelected);
+    }
+  }
 }
 
+function drawImage() {
+  const image = getCurrentImage();
+  imgContainer.src = image;
+}
+
+const imgContainer = document.getElementById('fill-image');
+const prevArrow = document.getElementById('left-arrow');
+const nextArrow = document.getElementById('right-arrow');
+const circlesContainer = document.getElementById('circles');
+
+let autoNext = 0;
+
+function stepping() {
+  clearInterval(autoNext);
+  autoNext = setInterval(() => {
+    nextImage();
+    drawImage();
+    drawCircles();
+  }, 3000);
+}
+
+function render() {
+  drawImage();
+  drawCircles();
+  stepping();
+}
+
+prevArrow.addEventListener('click', () => {
+  previousImage();
+  render();
+});
+
+nextArrow.addEventListener('click', () => {
+  nextImage();
+  render();
+});
+
+circlesContainer.addEventListener('click', event => {
+  for (let i = 0; i < circlesContainer.children.length; i++) {
+    if (event.target === circlesContainer.children[i]) {
+      currentPosition = i;
+      break;
+    }
+  }
+  render();
+});
+
+render();
