2377000b66edbd04ac93681d05411e7bb29c7cad
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-06-22 15:24:31 -0700

    Object manipulation 2.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,11 @@
 /* exported omit */
+function omit(source, keys) {
+  var results = {};
+  for (var i = 0; i < keys.length)
+}
+
+// source = {foo: 1, bar: 2, baz: 3}
+// keys = ['foo', 'baz']
+// results = {}
+// results.bar = source.bar
+//return {bar: 2}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,10 +1,18 @@
 /* exported pick */
 function pick(source, keys) {
-  var result = {};
-  for (keys in source) {
-    if (keys.includes(keys)) {
-      result[keys] = source[keys];
+  var results = {};
+  for (var i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    if (source[key] !== undefined) {
+      results[key] = source[key];
     }
   }
-  return result;
+  return results;
 }
+
+// source = {foo: 1, bar: 2, baz: 3}
+// keys = ['foo', 'baz']
+// results = {}
+// results.foo = source.foo
+// results ['baz'] = source['baz']
+// return {foo: 1, baz: 3}
