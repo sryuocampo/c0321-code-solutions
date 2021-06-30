931a58d1e9291e126c5c3689929beb7ccd3f3c91
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-06-30 10:52:05 -0700

    Completed object manipulation 2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -2,45 +2,12 @@
 
 function defaults(target, source) {
   const sourceArray = Object.keys(source);
-  const targetArray = Object.keys(target);
 
   for (let i = 0; i < sourceArray.length; i++) {
     const sourceArrayKey = sourceArray[i];
-    for (let i = 0; i < targetArray.length; i++) {
-      const targetArrayKey = targetArray[i];
-      if (targetArrayKey !== sourceArrayKey) {
-        Object.assign(target, source);
-        console.log(target);
-      } else if (targetArrayKey.length === 0) {
-        target.push(source);
-      }
+    if (target[sourceArrayKey] === undefined) {
+      target[sourceArrayKey] = source[sourceArrayKey];
     }
   }
 }
 
-// target = {};
-// source = {
-// foo: 1,
-// bar: 2,
-// baz: 3
-// }
-
-// targetArray= []
-// sourceArray = ['foo', 'bar', 'baz']
-
-// []
-// sourceArray = 'foo'
-// sourceArray = 'bar'
-// sourceArray = 'baz'
-
-// [] !== 'foo'
-// [] !== 'bar'
-// []  !== 'baz'
-
-
-
-// target = {
-// foo: 1,
-// bar: 2,
-// baz: 3
-// }
